becf6e89b0155fff2454dbf2caede6ba49c809bf
Abdelrazek <aalrazek02@gmail.com>
2024-01-31 17:59:51 +0200

    create failure handler file

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,13 +1,35 @@
+import 'package:dio/dio.dart';
+
 abstract class Failure{
   final String errorMassege;
 
-  Failure({required this.errorMassege});
+  Failure( this.errorMassege);
 }
 
 class ServerFailure extends Failure{
-  ServerFailure({required super.errorMassege});
+  ServerFailure( super.errorMassege);
 
-  
+  factory ServerFailure.fromDioException(DioException dioException){
+    switch(dioException.type){
+
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure(dioException.response!.data['error']['message']);
+      case DioExceptionType.badResponse:
+        return ServerFailure(dioException.response!.data['error']['message']);
+      case DioExceptionType.cancel:
+        return ServerFailure('requst canceled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('connection error, please try again letar');
+      case DioExceptionType.unknown:
+        return ServerFailure('unexpected error, please try again!');
+    }
+  }
 
 }
 
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:books_app/core/utils/api_service.dart';
 import 'package:books_app/features/home/data/models/book_model/book_model.dart';
 import 'package:books_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -13,20 +14,39 @@ class HomeRepoImpl implements HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
     try {
       var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&Sorting=newset&q=subject:Programming');
+      List<BookModel>books=[];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+      return right(books);
+    } catch (error) {
+      if(error is DioException){
+        return left(ServerFailure.fromDioException(error));
+      }
+      else{
+        return left(ServerFailure(error.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks()async {
+    try {
+      var data = await apiService.get(
           endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
       List<BookModel>books=[];
       for (var book in data['items']) {
         books.add(BookModel.fromJson(book));
       }
       return right(books);
-    } catch (e) {
-      return left(ServerFailure(errorMassege: e.toString()));
+    } catch (error) {
+      if(error is DioException){
+        return left(ServerFailure.fromDioException(error));
+      }
+      else{
+        return left(ServerFailure(error.toString()));
+      }
     }
-  }
-
-  @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
   }
 }
